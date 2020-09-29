b41366d2ac0a8e144605691c6c52da6acd6a118d
Jdowsett21 <james.dowsett.cooper@gmail.com>
2020-09-29 11:53:04 -0400

    isoloating heroku issue2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-// require('./startup/routes')(app);
-// require('./startup/db')();
+require('./startup/routes')(app);
+require('./startup/db')();
 
 const port = process.env.PORT || 8000;
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
