2a74c228f70a19af6e399aeef2510fd28ee7a433
Jdowsett21 <james.dowsett.cooper@gmail.com>
2020-09-29 11:04:20 -0400

    preparing for heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 require('./startup/routes')(app);
 require('./startup/db')();
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 
 module.exports = server;
