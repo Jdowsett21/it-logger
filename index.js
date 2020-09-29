30b3f2f3d58c8716e07c0f644703a71fada5d0dd
Jdowsett21 <james.dowsett.cooper@gmail.com>
2020-09-29 12:53:53 -0400

    heroku attempt5

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 
 require('./startup/routes')(app);
-require('./startup/db')();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
 const port = process.env.PORT || 8000;
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 
+require('./startup/db')();
 module.exports = server;
--- startup/routes.js
+++ startup/routes.js
@@ -4,37 +4,39 @@ const techs = require('../routes/techs');
 const auth = require('../routes/auth');
 const users = require('../routes/users');
 //middleware
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
-// const cookieParser = require('cookie-parser');
-// const csrf = require('csurf');
-// const jwt = require('express-jwt');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const csrf = require('csurf');
+const jwt = require('express-jwt');
 
-// //custom middleware
-// const attachUser = require('../middleware/attachUser');
+//custom middleware
+const attachUser = require('../middleware/attachUser');
 
-// const csrfProtection = csrf({ cookie: true });
+const csrfProtection = csrf({ cookie: true });
 
-// const verifyToken = jwt({
-//   secret: process.env.JWT_SECRET,
-//   iss: 'api.it-logger',
-//   aud: 'api.it-logger',
-//   algorithms: ['HS256'],
-//   getToken: (req) => req.cookies.token,
-// });
+const verifyToken = jwt({
+  secret: process.env.JWT_SECRET,
+  iss: 'api.it-logger',
+  aud: 'api.it-logger',
+  algorithms: ['HS256'],
+  getToken: (req) => req.cookies.token,
+});
 
 //function to run all middleware in index.js
 module.exports = function (app) {
-  // app.use(cookieParser());
-  // app.use(cors());
-  // app.use(bodyParser.urlencoded({ extended: false }));
-  // app.use(bodyParser.json());
-
+  app.use(cookieParser());
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  }
   app.use('/api/auth', auth);
-  // app.use(attachUser);
-  // app.use(verifyToken);
-  // app.use(csrfProtection);
-  // app.use('/api/users', users);
-  // app.use('/api/techs', techs);
-  // app.use('/api/logs', logs);
+  app.use(attachUser);
+  app.use(verifyToken);
+  app.use(csrfProtection);
+  app.use('/api/users', users);
+  app.use('/api/techs', techs);
+  app.use('/api/logs', logs);
 };
