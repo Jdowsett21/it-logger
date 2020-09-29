16cfe4d8c741f5d5cc4df883fdd33c2cde4dbd3f
Jdowsett21 <james.dowsett.cooper@gmail.com>
2020-09-29 14:06:10 -0400

    heroku attempt10

--- startup/routes.js
+++ startup/routes.js
@@ -33,9 +33,9 @@ module.exports = function (app) {
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
   }
-  app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+  // app.get('*', (request, response) => {
+  //   response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  // });
   app.use('/api/auth', auth);
   app.use(attachUser);
   app.use(verifyToken);
